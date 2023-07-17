f12523238e25ef503469f8e530d36788f6bf2802
rezamohdev <mohammadireza29@gmail.com>
2023-07-16 22:03:47 -0400

    User is done

--- controllers/users.js
+++ controllers/users.js
@@ -7,20 +7,21 @@ const getUsers = (req, res) => {
     User.find({}).then((users) => {
         res.status(200).send(users);
     }).catch((err) => {
-        res.status(500).send({ message: 'get users faild', err })
+        res.status(500).send({ message: 'get users faild, error : ', err })
     })
 }
 
 // GET /users/:userId - returns a user by _id
 const findUser = (req, res) => {
-    const { id } = req.params.id;
-    User.findById(id).
+    const { userId } = req.params;
+    User.findById(userId).
         then((data) => {
-            console.log(id);
+            console.log(userId);
             console.log(data); res.send(data)
         }).
         catch((err) => {
-            res.status(500).send('Failed creating user', err)
+            console.error(err);
+            res.status(500).send('Failed finding user by id, error :', err)
         })
 }
 // POST /users — creates a new user
--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const { getUsers, createUser, findUser } = require('../controllers/users');
 router.get('/users', getUsers);
 
 // GET /users/:userId - returns a user by _id
-router.get('/users/:userId ', findUser);
+router.get('/users/:userId', findUser);
 
 // POST /users — creates a new user
 router.post('/users', createUser)
