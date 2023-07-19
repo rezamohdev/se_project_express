fef80e67af38d7ceec37cb9611e907d681a0513c
rezamohdev <mohammadireza29@gmail.com>
2023-07-19 14:39:41 -0400

    test

--- controllers/likes.js
+++ controllers/likes.js
@@ -1,23 +1,40 @@
 const clothingItem = require('../models/clothingItems');
+const { ERROR_400, ERROR_500, ERROR_404 } = require('../utils/errors');
 
+
+
+const handleError = (req, res, error) => {
+    console.error(`error is : ${error}`)
+    if (error.name === 'ValidationError' || error.name === 'AssertionError') {
+        return res.status(ERROR_400).send({
+            message: 'Passed invalid data !'
+        });
+    } else if (error.name === 'CastError') {
+        return res.status(ERROR_400).send({
+            message: 'The request is sent to a none existense resource!'
+        });
+    }
+    return res.status(ERROR_404).send({
+        message: 'Passed invalid data!'
+    });
+}
 // PUT / items /: itemId / likes — like an item
 module.exports.likeItem = (req, res) => {
     clothingItem.findByIdAndUpdate(
         req.params.itemId,
-        { $addToSet: { likes: req.user._id } }, // add _id to the array if it's not there yet
-        { new: true },
-        (err, updatedItem) => {
-            console.log("test");
-            if (err) {
-                return res.status(400).json({ message: "An error occurred while liking the item." });
-            }
-            return res.header('Content-Type', 'application/json').json(updatedItem);
-        }
+        { $addToSet: { likes: req.user._id } },
+        { new: true }
     )
+        .orFail()
+        .then((data) => {
+            return res.status(200).send(data);
+        }).catch((err) => {
+            handleError(req, res, err);
+        })
 }
 
 // DELETE / items /: itemId / likes — unlike an item
-module.exports.dislikeItem = (req, res) => ClothingItem.findByIdAndUpdate(
+module.exports.dislikeItem = (req, res) => clothingItem.findByIdAndUpdate(
     req.params.itemId,
     { $pull: { likes: req.user._id } }, // remove _id from the array
     { new: true },
