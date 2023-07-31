b62d61aee539feeb10fb73feeecccc2451635aa9
rezamohdev <mohammadireza29@gmail.com>
2023-07-31 14:58:56 -0400

    project fix

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = require('express').Router();
 const { getCurrentUser, updateProfile } = require('../controllers/users');
 const auth = require('../middlewares/auth');
 
-router.get('/users/me', auth(), getCurrentUser);
+router.get('/users/me', auth, getCurrentUser);
 router.patch('/users/me', auth, updateProfile);
 
 module.exports = router;
